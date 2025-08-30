44bd0ec0cdc858720f33dfb09de897ebee93a6a4
Simon Jones <sigavjones@gmail.com>
2025-08-30 17:26:22 +0100

    Implemented basic react-router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.scss'
 import Login from './pages/login/login'
+import Dashboard from './pages/dashboard/dashboard'
+import { Routes, Route } from 'react-router'
 
 function App() {
    return (
-      <>
-         <Login />
-      </>
+      <Routes>
+         <Route path="/" element={<Login />} />
+         <Route path="/login" element={<App />} />
+         <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
    )
 }
 
