cc4012bfbd83bd88a61ff2abff0146777a8a3a35
Simon Jones <sigavjones@gmail.com>
2025-09-11 13:53:29 +0100

    Routing element fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import './App.scss'
+import Layout from './components/Layout/Layout'
 import Login from './pages/login/login'
 import Dashboard from './pages/dashboard/dashboard'
 import { Routes, Route } from 'react-router'
 
 function App() {
    return (
-      <Routes>
-         <Route path="/" element={<Login />} />
-         <Route path="/login" element={<App />} />
-         <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <Layout>
+         <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+         </Routes>
+      </Layout>
    )
 }
 
